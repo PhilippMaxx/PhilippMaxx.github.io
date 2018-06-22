c559a312965cf23c3bb5442fb6c9884bda2b1aa1
Philipp Möhl <philipp9419@hotmail.de>
2018-06-22 15:49:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
+<?php ?>
 <!DOCTYPE html>
 <html>
 <body>
-<h1>Hello World</h1>
-<p>I'm hosted with GitHub Pages.</p>
+<form action="">
+  <input type="text" name="location">
+  <button type="submit">submit</button>
+</form>
 </body>
 </html>
